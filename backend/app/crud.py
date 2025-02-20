8ab0941aa8045a6f098841646ae22e97bf458126
ArtemKhoda <khodakivskiya@gmail.com>
2025-02-20 09:27:49 -0500

    - add submission db schema and CRUD functionalities - integrate with frontend for get api - test complete via Swagger

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -4,7 +4,8 @@ from typing import Any
 from sqlmodel import Session, select
 
 from app.core.security import get_password_hash, verify_password
-from app.models import Item, ItemCreate, User, UserCreate, UserUpdate
+from app.models import Item, ItemCreate, User, UserCreate, UserUpdate, Submission, SubmissionCreate, SubmissionUpdate
+from fastapi import HTTPException, status
 
 
 def create_user(*, session: Session, user_create: UserCreate) -> User:
@@ -52,3 +53,45 @@ def create_item(*, session: Session, item_in: ItemCreate, owner_id: uuid.UUID) -
     session.commit()
     session.refresh(db_item)
     return db_item
+
+def create_submission(db: Session, submission_data: SubmissionCreate) -> Submission:
+    new_submission = Submission(**submission_data.dict())
+    db.add(new_submission)
+    db.commit()
+    db.refresh(new_submission)
+    return new_submission
+
+
+def get_submission_by_id(db: Session, submission_id: uuid.UUID) -> Submission:
+    submission = db.get(Submission, submission_id)
+    if not submission:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Submission with ID {submission_id} not found",
+        )
+    return submission
+
+
+def get_submissions(db: Session, skip: int = 0, limit: int = 10) -> list[Submission]:
+    statement = select(Submission).offset(skip).limit(limit)
+    results = db.exec(statement).all()
+    return results
+
+
+def update_submission(
+    db: Session, submission_id: uuid.UUID, submission_data: SubmissionUpdate
+) -> Submission:
+    submission = get_submission_by_id(db, submission_id)
+    update_data = submission_data.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(submission, key, value)
+    db.add(submission)
+    db.commit()
+    db.refresh(submission)
+    return submission
+
+
+def delete_submission(db: Session, submission_id: uuid.UUID) -> None:
+    submission = get_submission_by_id(db, submission_id)
+    db.delete(submission)
+    db.commit()
